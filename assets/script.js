2e0b7d6254dda20180802a7e330a7a96509ffd71
Salwador Szurlej <sal.szu@gmail.com>
2017-07-26 22:00:10 -0400

    working on pdf portion

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,31 @@
-window.addEventListener('load',function(){
+window.addEventListener('load', function() {
   var welcome = document.querySelector('.greet'),
-      subtext = document.querySelector('.subTexts'),
-      follow  = document.querySelector('.followUs'),
-      social  = document.querySelectorAll('.socialIcon'),
-      portfolio = document.querySelector('.portfolio'),
-      delay = 1000;
+    subtext = document.querySelector('.subTexts'),
+    follow = document.querySelector('.followUs'),
+    social = document.querySelectorAll('.socialIcon'),
+    portfolio = document.querySelector('.portfolio'),
+    delay = 1000;
 
 
-  setTimeout(function(){welcome.style.top='0';},delay);
-  setTimeout(function(){subtext.style.bottom = '0%';},delay*2);
-  setTimeout(function(){subtext.style.bottom = '-100%';},delay*4);
-  setTimeout(function(){form.style.opacity='1';},delay*5);
-  setTimeout(function(){follow.style.bottom='0%';},delay*5.5);
+  setTimeout(function() {
+    welcome.style.top = '0';
+  }, delay);
+  setTimeout(function() {
+    subtext.style.bottom = '0%';
+  }, delay * 1.5);
+  // setTimeout(function(){subtext.style.bottom = '-100%';},delay*1.75);
+  setTimeout(function() {
+    form.style.opacity = '1';
+  }, delay * 2);
+  setTimeout(function() {
+    follow.style.bottom = '0%';
+  }, delay * 3);
   setTimeout(
-    function(){
-      social[0].style.marginTop='0px';
-      social[1].style.marginTop='0px';
-      social[2].style.marginTop='0px';
-    },delay*7
+    function() {
+      social[0].style.marginTop = '0px';
+      social[1].style.marginTop = '0px';
+      social[2].style.marginTop = '0px';
+    }, delay * 4
   );
 
 
